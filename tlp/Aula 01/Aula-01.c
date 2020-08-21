3d36d09607d5677869a368ede973d11ff46d086e
dcmunhoz <dc.munhoz@hotmail.com>
2020-08-21 17:57:13 -0300

    first commit with first class

---
+++ tlp/Aula 01/Aula-01.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int main(){
+//	
+//	int x = 10;
+//	int y = 2;
+//	int z;
+//	
+//	// Soma
+//	z = x+y;
+//	printf("A soma e: %d \n", z);
+//	
+//	// Subtra��o
+//	z = x - y;
+//	printf("A subtracao e: %d \n", z);
+//	
+//	// Divis�o
+//	z = x / y;
+//	printf("A divisao e: %d \n", z);
+//	
+//	// Multiplica��o
+//	z = x * y;
+//	printf("A multiplicacao e: %d \n", z);
+	
+	
+//	int a = 2;
+//	int b = 3;
+//	int resultado = a + b;
+//	
+//	printf("%d e a soma de %d com %d \n", resultado, a, b);
+
+
+	int valor;
+	
+	printf("====== BEM VINDO AO SISTEM ADS - UNIP 01 ====== \n \n");
+	
+	printf("Digite um valor numerico de entrada: ");
+	scanf("%d", &valor);
+	
+	printf("O valor que você digitasdsadsou foi: %d", valor);
+
+	// system("pause");
+	return 0;	
+}
